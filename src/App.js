67d1a098d09a904054a120880a08132df2492694
Nick Land <nicklandiam@gmail.com>
2022-01-27 18:52:30 -0700

    Changed login page template to match wireframe

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
               />
             </Routes>
           </BrowserRouter>
-        </React.Fragment>
+      </React.Fragment>
   );
 }
 
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -4,26 +4,53 @@ import { Link } from 'react-router-dom';
 class LoginPage extends Component {
     render() { 
         return (
-            <div className='container'>
-                <div className='row my-5'>
-                    <div className='col'>
-                        <Link to="/admin">
-                            <button className='btn btn-dark'>
-                                Admins
-                            </button>
-                        </Link>
+
+            <div className='container-fluid '>
+               <div className='row'>
+                    <div className='col-6'>
+                        <p>Left</p>
                     </div>
-                </div>
-                <div className='row my-5'>
-                    <div className='col'>
-                        <Link to="/employee">
-                            <button className='btn btn-dark'>
-                                Employees
-                            </button>
-                        </Link>
+                    <div className='col-6 d-flex justify-content-center'>
+                        <div className='row min-vh-100'>
+                            <div className='col d-flex align-items-center'>
+                                <div className="row flex-column">
+                                    <div className='col'>
+                                        <Link to="/admin">
+                                            <button className='btn btn-dark'>
+                                                Admins
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className='col'>
+                                        <Link to="/employee">
+                                            <button className='btn btn-dark'>
+                                                Employees
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+               </div>
             </div>
+
+            // <div className='container'>
+            //     <div className='row my-5'>
+            //         <div className='col'>
+            //             <Link to="/admin">
+            //                 <button className='btn btn-dark'>
+            //                     Admins
+            //                 </button>
+            //             </Link>
+            //         </div>
+            //     </div>
+            //     <div className='row my-5'>
+            //         <div className='col'>
+                        
+            //         </div>
+            //     </div>
+            // </div>
         );
     }
 }
