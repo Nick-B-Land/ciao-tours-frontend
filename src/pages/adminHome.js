36c63d3ab4334e3526dfc700bc1cba72af6abf53
Prince Bansal <61831166+pbsos38@users.noreply.github.com>
2022-02-14 15:28:27 -0700

    Add files via upload

--- src/pages/adminHome.js
+++ src/pages/adminHome.js
@@ -1,150 +1,140 @@
-import React, { Component, useState, useEffect } from 'react';
-import '../style/stylesheet.css';
-import EmployeeCard from '../components/employeeCard';
-import employeeController from '../controllers/employeeController';
-import TopAdminNav from '../components/topAdminNav';
-import BottomAdminNav from '../components/bottomAdminNav';
-import FlaggedDay from '../components/flaggedDay';
-import EmployeeHours from '../components/employeeHours';
-import { Link } from 'react-router-dom';
-import { empHoursData, flaggedDayData } from '../components/data';
+import React, { Component, useState, useEffect } from "react";
+import "../style/stylesheet.css";
+import EmployeeCard from "../components/employeeCard";
+import employeeController from "../controllers/employeeController";
+import TopAdminNav from "../components/topAdminNav";
+import BottomAdminNav from "../components/bottomAdminNav";
+import TopNavWrapper from "../functionalComponents/topNavWrapper";
 
 class AdminHome extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            employeeList: [],
-            employeesLoaded: false
+	constructor(props) {
+		super(props);
+		this.state = {
+			employeeList: [],
+			employeesLoaded: false,
+		};
+	}
 
-        };
-    }
+	componentDidMount() {
+		employeeController.getEmployees().then((employees) => {
+			this.setState({ employeeList: employees.data, employeesLoaded: true });
+		});
+	}
 
-    componentDidMount() {
-        employeeController.getEmployees().then(
-            employees => {
+	// renderEmployees = () => {
+	// 	console.log("render Employees fired");
+	// 	console.log(this.state.employeeList);
+	// 	return this.state.employeeList.map((e) => {
+	// 		<EmployeeCard
+	// 			key={e.id}
+	// 			firstName={e.firstName}
+	// 			lastName={e.lastName}
+	// 			jobTitle={e.jobTitle}
+	// 		/>
+	// 	});
+	// };
 
-                this.setState({ employeeList: employees.data, employeesLoaded: true });
+	render() {
+		var adminName = "Administrator";
+		var monthName = [
+			"JAN",
+			"FEB",
+			"MAR",
+			"APR",
+			"MAY",
+			"JUN",
+			"JUL",
+			"AUG",
+			"SEP",
+			"OCT",
+			"NOV",
+			"DEC",
+		];
+		var current = new Date();
+		var month = `${monthName[current.getMonth()]}`;
+		var day = `${current.getDate()}`;
+		var year = `${current.getFullYear()}`;
 
-            }
-        );
-    }
+		return (
+			<div className="container-fluid p-0 adminHomePage">
+				<div className="row d-flex">
+					<TopNavWrapper />
+					<BottomAdminNav />
+				</div>
+				<div className="row">
+					<div className="col-2"></div>
+					<div className="col-8 innerAdmin">
+						<div className="row">
+							<div className="col min-vh-100 adminContent">
+								<h1>Welcome, {adminName}</h1>
 
-    renderEmployees = () => {
-        console.log("render Employees fired");
-        console.log(this.state.employeeList);
-        return this.state.employeeList.map(e => {
+								<div className="flaggedDays">
+									<h2>Flagged Days</h2>
+									<p>
+										Below is a list of days or expenses that have been flagged.
+									</p>
+									<table>
+										<thead>
+											<tr>
+												<th>Name</th>
+												<th>Date</th>
+												<th>Issue</th>
+												<th>Action</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>Francis Mack</td>
+												<td className="flagDate">Jan 10</td>
+												<td>Unsure if hours are correct</td>
+												<td>
+													<button>Resolve</button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
 
-            <EmployeeCard
-                key={e.id}
-                firstName={e.firstName}
-                lastName={e.lastName}
-                jobTitle={e.jobTitle}
-            />
-        })
-    }
+								<div className="todaysDate">
+									<h2>Today's Date</h2>
+									<p className="curMonth">{month}</p>
+									<p className="curDay">{day}</p>
+									<p className="curYear">{year}</p>
+								</div>
 
-    render() {
-        var adminName = 'Administrator';
-        var monthName = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-        var current = new Date();
-        var month = `${monthName[current.getMonth()]}`;
-        var day = `${current.getDate()}`;
-        var year = `${current.getFullYear()}`;
+								<div className="listOfEmp">
+									<h2>List of Employees</h2>
+									<table>
+										<thead>
+											<tr>
+												<th>Name</th>
+												<th>Hours</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>Francis Mack</td>
+												<td>22</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
 
-        return (
-            <div className='container-fluid p-0 adminHomePage'>
-                <div className='row d-flex'>
-                    <TopAdminNav />
-                    <BottomAdminNav />
-                </div>
-                <div className='row'>
-                    <div className='col-2'></div>
-                    <div className='col-8 innerAdmin'>
-                        <div className='row'>
-                            <div className='col mt-2 min-vh-100'>
-                                <h1 className='mb-2'>Welcome, {adminName}</h1>
-                                <div className='container'>
-                                    <div className='row'>
-                                        <div className='col-md-9 p-2'>
-                                            <div className='p-3 bg-white pb-4 border rounded-3'>
-                                                <h2 className='text-center'>Flagged Days</h2>
-                                                <p>Below is a list of days or expenses that have been flagged.</p>
-                                                <table className="table">
-                                                    <thead>
-                                                        <tr>
-                                                            <th scope="col">Name</th>
-                                                            <th scope="col">Date</th>
-                                                            <th scope="col">Issue</th>
-                                                            <th scope="col">Action</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {flaggedDayData.map(({ firstName, lastName, date, issue }) => (
-                                                            <FlaggedDay
-                                                                firstName={firstName}
-                                                                lastName={lastName}
-                                                                date={date}
-                                                                issue={issue}
-                                                            />
-                                                        ))}
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                        <div className='col-md-3 p-2 text-center'>
-                                            <div className='p-3 bg-white border rounded-3'>
-                                                <h6>Today's Date</h6>
-                                                <p className='h2 m-0'>{month}</p>
-                                                <p className='display-1 m-0'>{day}</p>
-                                                <p className='h2 m-0'>{year}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className='row'>
-                                        <div className='col-md-6 p-2'>
-                                            <div className='p-3 bg-white border rounded-3'>
-                                                <h2>List of Employees</h2>
-                                                <table className="table">
-                                                    <thead>
-                                                        <tr>
-                                                            <th scope="col">Name</th>
-                                                            <th scope="col">Hours</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {empHoursData.map(({ firstName, lastName, hours }) => (
-                                                            <EmployeeHours
-                                                                firstName={firstName}
-                                                                lastName={lastName}
-                                                                hours={hours}
-                                                            />
-                                                        ))}
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                        <div className='col-md-6 p-2'>
-                                            <div className='p-3 bg-white border rounded-3'>
-                                                <div className='d-flex justify-content-center'>
-                                                    <Link to="/newEmployee">
-                                                        <button type="button" className="btn btn-success btn-lg p-5 pt-2 pb-2">
-                                                            <h2>Add New Employee</h2>
-                                                        </button>
-                                                    </Link>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+								<div className="empty"></div>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-2"></div>
+							<div className="col"></div>
+						</div>
+					</div>
+					<div className="col-2"></div>
+				</div>
 
-                {/* {this.state.employeesLoaded ? this.renderEmployees() : <h3>Loading</h3>} */}
-            </div>
-        );
-    }
+				{/* {this.state.employeesLoaded ? this.renderEmployees() : <h3>Loading</h3>} */}
+			</div>
+		);
+	}
 }
 
 export default AdminHome;
--- src/pages/employeeHome.js
+++ src/pages/employeeHome.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import TopNav from '../components/topNav';
-import BottomEmpNav from '../components/bottomEmpNav';
+import React, { Component } from "react";
+// import TopNav from "../components/topNav";
+import BottomEmpNav from "../components/bottomEmpNav";
+import TopNavWrapper from "../functionalComponents/topNavWrapper";
 
 class EmployeeHome extends Component {
-    render() {
-        var empName = 'Employee'; 
-        return (
-            <div className='container-fluid p-0 employeeHomePage'>
-                <div className='row d-flex'>
-                    <TopNav />
-                    <BottomEmpNav />
-                </div>
-                <div className='row'>
-                    <div className='col-2'>
-                    </div>
-                    <div className='col-8 innerAdmin'>
-                        <div className='row'>
-                            <div className='col min-vh-100'>
-                                <h1>Welcome, {empName}</h1>
-                            </div>
-                        </div>
-                        <div className='row'>
-                            <div className='col-2'></div>
-                            <div className='col'></div>
-                        </div>
-                    </div>
-                    <div className='col-2'></div>
-                </div>
+	render() {
+		var empName = "Employee";
+		return (
+			<div className="container-fluid p-0 employeeHomePage">
+				<div className="row d-flex">
+					<TopNavWrapper />
+					<BottomEmpNav />
+				</div>
+				<div className="row">
+					<div className="col-2"></div>
+					<div className="col-8 innerAdmin">
+						<div className="row">
+							<div className="col min-vh-100">
+								<h1>Welcome, {empName}</h1>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-2"></div>
+							<div className="col"></div>
+						</div>
+					</div>
+					<div className="col-2"></div>
+				</div>
 
-                {/* {this.state.employeesLoaded ? this.renderEmployees() : <h3>Loading</h3>} */}
-            </div>
-        );
-    }
+				{/* {this.state.employeesLoaded ? this.renderEmployees() : <h3>Loading</h3>} */}
+			</div>
+		);
+	}
 }
- 
+
 export default EmployeeHome;
