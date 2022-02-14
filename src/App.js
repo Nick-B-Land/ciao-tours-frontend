6efa918d897d3afd6dd286050e53cfc1a8a728e1
Prince Bansal <61831166+pbsos38@users.noreply.github.com>
2022-02-14 15:25:43 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,58 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LoginPage from './pages/loginPage';
-import AdminHome from './pages/adminHome';
-import EmployeeHome from './pages/employeeHome';
+// import LoginPage from "./pages/loginPage";
+import Myinfo from "./pages/employeeInfo";
+import EditMyinfo from "./pages/editEmployeeInfo";
+import AdminHome from "./pages/adminHome";
+import EmployeeHome from "./pages/employeeHome";
 import AdminEmployees from "./pages/adminEmployees";
 import AdminReports from "./pages/adminReports";
 import Information from "./pages/information";
-import NewEmployee from "./pages/newEmployee";
-
+import LoginWrapper from "./functionalComponents/loginWrapper";
 function App() {
-  return (
-    <React.Fragment>
-          <BrowserRouter>
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={<LoginPage />}
-              />  
-              <Route
-                exact
-                path="/admin"
-                element={<AdminHome />}
-              />
-              <Route
-                exact
-                path="/employee"
-                element={<EmployeeHome />}
-              />
-              <Route
-                exact
-                path="/adminEmployees"
-                element={<AdminEmployees />}
-              />
-              <Route
-                exact
-                path="/newEmployee"
-                element={<NewEmployee />}
-              />
-              <Route
-                exact
-                path="/adminReports"
-                element={<AdminReports />}
-              />
-              <Route
-                exact
-                path="/information"
-                element={<Information />}
-              />
-            </Routes>
-          </BrowserRouter>
-      </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path="/login" element={<LoginWrapper />} />
+					<Route exact path="/admin" element={<AdminHome />} />
+					<Route exact path="/employee" element={<EmployeeHome />} />
+					<Route exact path="/adminEmployees" element={<AdminEmployees />} />
+					<Route exact path="/adminReports" element={<AdminReports />} />
+					<Route exact path="/myinfo" element={<Myinfo/>} />
+					<Route exact path="/editEmployeeInfo" element={<EditMyinfo />} />
+					<Route exact path="/" element={<Information />} />
+				</Routes>
+			</BrowserRouter>
+		</React.Fragment>
+	);
 }
 
 export default App;
