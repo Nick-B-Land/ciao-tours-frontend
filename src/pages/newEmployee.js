5e49f593476b844673c63e69817972f9ec0035b2
Nick <59325182+Nick-B-Land@users.noreply.github.com>
2022-04-07 10:40:28 -0600

    Update newEmployee.js

--- src/pages/newEmployee.js
+++ src/pages/newEmployee.js
@@ -6,11 +6,10 @@ import employeeController from "../controllers/employeeController";
 import { Link } from "react-router-dom";
 import userAccountController from "../controllers/userAccountController";
 
-
 class NewEmployee extends Component {
 	constructor(props) {
 		super(props);
-		this.hiddenFileBtn=React.createRef();
+		this.hiddenFileBtn = React.createRef();
 		this.state = {
 			firstname: "",
 			lastname: "",
@@ -21,7 +20,7 @@ class NewEmployee extends Component {
 			enddate: "",
 			hourlywage: "",
 			monthlysalary: "",
-			file:null,
+			file: null,
 			isadmin: false,
 			isbookkeeper: false,
 			institutionid: "",
@@ -42,7 +41,7 @@ class NewEmployee extends Component {
 			showInstituiteIDError: true,
 			showBANError: true,
 			showTransIDError: true,
-			fileSelected:false,
+			fileSelected: false,
 
 			UsernameError: "",
 			PasswordError: "",
@@ -56,66 +55,77 @@ class NewEmployee extends Component {
 			InstituiteIDError: "",
 			BANError: "",
 			TransIDError: "",
-			
+
 			error: false,
-			errorMessage: ""
+			errorMessage: "",
 		};
 	}
 
 	handleFirstnameInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showFirstnameError: true, FirstnameError: "First Name can't be empty." })
-
+			this.setState({
+				showFirstnameError: true,
+				FirstnameError: "First Name can't be empty.",
+			});
 		} else {
-			this.setState({ showFirstnameError: false, FirstnameError: "" })
+			this.setState({ showFirstnameError: false, FirstnameError: "" });
 		}
 		this.setState({ firstname: e.target.value });
-
 	};
 	handleLastnameInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showLastnameError: true, LastnameError: "Last Name can't be empty." })
+			this.setState({
+				showLastnameError: true,
+				LastnameError: "Last Name can't be empty.",
+			});
 		} else {
-			this.setState({ showLastnameError: false, LastnameError: "" })
+			this.setState({ showLastnameError: false, LastnameError: "" });
 		}
 		this.setState({ lastname: e.target.value });
-
 	};
 
 	handleAddressInput = (e) => {
-
 		if (e.target.value === "") {
-			this.setState({ showAddressError: true, AddressError: "Address can't be empty." })
+			this.setState({
+				showAddressError: true,
+				AddressError: "Address can't be empty.",
+			});
 		} else {
-			this.setState({ showAddressError: false, AddressError: "" })
+			this.setState({ showAddressError: false, AddressError: "" });
 		}
 		this.setState({ address: e.target.value });
 	};
 
 	handleCityInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showCityError: true, CityError: "City can't be empty." })
+			this.setState({ showCityError: true, CityError: "City can't be empty." });
 		} else {
-			this.setState({ showCityError: false, CityError: "" })
+			this.setState({ showCityError: false, CityError: "" });
 		}
 		this.setState({ city: e.target.value });
 	};
 
 	handleEmailInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showEmailError: true, EmailError: "Email can't be empty." })
+			this.setState({
+				showEmailError: true,
+				EmailError: "Email can't be empty.",
+			});
 		} else {
-			this.setState({ showEmailError: false, EmailError: "" })
+			this.setState({ showEmailError: false, EmailError: "" });
 		}
 		this.setState({ email: e.target.value });
 	};
 
 	handleStartdateInput = (e) => {
-		console.log(e.target.value)
+		console.log(e.target.value);
 		if (e.target.value === "") {
-			this.setState({ showStartDateError: true, StartDateError: "Start Date Invalid" })
+			this.setState({
+				showStartDateError: true,
+				StartDateError: "Start Date Invalid",
+			});
 		} else {
-			this.setState({ showStartDateError: false, StartDateError: "" })
+			this.setState({ showStartDateError: false, StartDateError: "" });
 		}
 		this.setState({ startdate: e.target.value });
 	};
@@ -134,31 +144,42 @@ class NewEmployee extends Component {
 
 	handleInstitutionIdInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showInstituiteIDError: true, InstituiteIDError: "Institution ID can't be empty." })
+			this.setState({
+				showInstituiteIDError: true,
+				InstituiteIDError: "Institution ID can't be empty.",
+			});
 		} else if (e.target.value.length > 3) {
-			this.setState({ showInstituiteIDError: true, InstituiteIDError: "Institution ID can't be more than 3 characters." })
+			this.setState({
+				showInstituiteIDError: true,
+				InstituiteIDError: "Institution ID can't be more than 3 characters.",
+			});
 		} else {
-			this.setState({ showInstituiteIDError: false, InstituiteIDError: "" })
+			this.setState({ showInstituiteIDError: false, InstituiteIDError: "" });
 		}
 
 		this.setState({ institutionid: e.target.value });
 	};
 
 	handleBankAccountNumInput = (e) => {
-
 		if (e.target.value === "") {
-			this.setState({ showBANError: true, BANError: "Account Number can't be empty." })
+			this.setState({
+				showBANError: true,
+				BANError: "Account Number can't be empty.",
+			});
 		} else {
-			this.setState({ showBANError: false, BANError: "" })
+			this.setState({ showBANError: false, BANError: "" });
 		}
 		this.setState({ bankaccountnum: e.target.value });
 	};
 
 	handleTransitIdInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showTransIDError: true, TransIDError: "Transit ID can't be empty." })
+			this.setState({
+				showTransIDError: true,
+				TransIDError: "Transit ID can't be empty.",
+			});
 		} else {
-			this.setState({ showTransIDError: false, TransIDError: "" })
+			this.setState({ showTransIDError: false, TransIDError: "" });
 		}
 
 		this.setState({ transitid: e.target.value });
@@ -166,9 +187,12 @@ class NewEmployee extends Component {
 
 	handleUsernameInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showUsernameError: true, UsernameError: "Username can't be empty." })
+			this.setState({
+				showUsernameError: true,
+				UsernameError: "Username can't be empty.",
+			});
 		} else {
-			this.setState({ showUsernameError: false, UsernameError: "" })
+			this.setState({ showUsernameError: false, UsernameError: "" });
 		}
 
 		this.setState({ username: e.target.value });
@@ -176,32 +200,35 @@ class NewEmployee extends Component {
 
 	handlePasswordInput = (e) => {
 		if (e.target.value === "") {
-			this.setState({ showPasswordError: true, PasswordError: "Password can't be empty." })
+			this.setState({
+				showPasswordError: true,
+				PasswordError: "Password can't be empty.",
+			});
 		} else {
-			this.setState({ showPasswordError: false, PasswordError: "" })
+			this.setState({ showPasswordError: false, PasswordError: "" });
 		}
 		this.setState({ password: e.target.value });
 	};
 
 	handleRoleInput = (e) => {
 		if (e.target.value === "Select") {
-			this.setState({ showRoleError: true, RoleError: "Selected Role in invalid." })
+			this.setState({
+				showRoleError: true,
+				RoleError: "Selected Role in invalid.",
+			});
 		} else {
-			this.setState({ showRoleError: false, RoleError: "" })
+			this.setState({ showRoleError: false, RoleError: "" });
 		}
 		this.setState({ roleText: e.target.value });
 	};
-	
-	uploadFile =() => {
-		this.hiddenFileBtn.current.click();
 
+	uploadFile = () => {
+		this.hiddenFileBtn.current.click();
 	};
 	saveFile = async (event) => {
-		this.state({file:event.target.files[0]});
-		this.state({fileSelected:true});
-	
+		this.state({ file: event.target.files[0] });
+		this.state({ fileSelected: true });
 	};
-
 
 	handleNewEmployee = async () => {
 		// need to add in proper handling of is admin/bookkeeper - can't be a bool
@@ -210,7 +237,8 @@ class NewEmployee extends Component {
 		//should select and add roles from predefined list, this is hacky way for now
 		//can only add one role via this method
 		// this.setState({ roles: [...this.state.roles, this.state.roleText] });
-		if (this.state.showUsernameError === true ||
+		if (
+			this.state.showUsernameError === true ||
 			this.state.showRoleError === true ||
 			this.state.startdate === true ||
 			this.state.showPasswordError === true ||
@@ -224,9 +252,11 @@ class NewEmployee extends Component {
 			this.state.showTransIDError === true
 		) {
 			this.setState({ error: true, errorMessage: "Please Complete the form." });
-			setInterval(() => this.setState({ error: false, errorMessage: "" }), 4000);
+			setInterval(
+				() => this.setState({ error: false, errorMessage: "" }),
+				4000
+			);
 		} else {
-
 			let employeeObj = {
 				employeeId: "",
 				firstName: this.state.firstname,
@@ -248,13 +278,15 @@ class NewEmployee extends Component {
 			//let test = JSON.stringify(employeeObj);
 			//console.log(test);
 
-			let employeeResponse = await employeeController.createEmployee(employeeObj);
+			let employeeResponse = await employeeController.createEmployee(
+				employeeObj
+			);
 
 			console.log(employeeResponse);
 
 			let userObj = {
 				username: this.state.username,
-				employeeID: employeeResponse.data.employeeId,
+				employeeID: employeeObj,
 				password: this.state.password,
 				roles: this.state.roles,
 			};
@@ -270,9 +302,37 @@ class NewEmployee extends Component {
 	};
 
 	render() {
-		const { showUsernameError, showPasswordError, showFirstnameError, showLastnameError, showRoleError, showAddressError, showCityError, showEmailError
-			, showStartDateError, showInstituiteIDError, showBANError, showTransIDError, UsernameError,hiddenFileBtn,
-			PasswordError, RoleError, FirstnameError, LastnameError, AddressError, CityError, EmailError, StartDateError, InstituiteIDError, BANError, TransIDError, error, errorMessage,file, fileSelected } = this.state;
+		const {
+			showUsernameError,
+			showPasswordError,
+			showFirstnameError,
+			showLastnameError,
+			showRoleError,
+			showAddressError,
+			showCityError,
+			showEmailError,
+			showStartDateError,
+			showInstituiteIDError,
+			showBANError,
+			showTransIDError,
+			UsernameError,
+			hiddenFileBtn,
+			PasswordError,
+			RoleError,
+			FirstnameError,
+			LastnameError,
+			AddressError,
+			CityError,
+			EmailError,
+			StartDateError,
+			InstituiteIDError,
+			BANError,
+			TransIDError,
+			error,
+			errorMessage,
+			file,
+			fileSelected,
+		} = this.state;
 
 		return (
 			<div className="container-fluid p-0 adminEmployeesPage">
@@ -291,7 +351,9 @@ class NewEmployee extends Component {
 							<h1>Create New Employee</h1>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Username:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Username:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -299,13 +361,16 @@ class NewEmployee extends Component {
 									onChange={this.handleUsernameInput}
 								/>
 								<div className="row errorText">
-									{showUsernameError && <div className="error "> {UsernameError} </div>}
+									{showUsernameError && (
+										<div className="error "> {UsernameError} </div>
+									)}
 								</div>
 							</div>
-
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Password:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Password:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -313,15 +378,21 @@ class NewEmployee extends Component {
 									onChange={this.handlePasswordInput}
 								/>
 								<div className="row errorText">
-									{showPasswordError && <div className="error"> {PasswordError} </div>}
+									{showPasswordError && (
+										<div className="error"> {PasswordError} </div>
+									)}
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Roles:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Roles:
+							</div>
 							<div className="col dropdown newEmployeeRow">
-
-								<select value={this.state.roleText} onChange={this.handleRoleInput}>
+								<select
+									value={this.state.roleText}
+									onChange={this.handleRoleInput}
+								>
 									<option value="Select">Select</option>
 									<option value="Admin">Admin</option>
 									<option value="Bookkeeper">Bookkeeper</option>
@@ -334,20 +405,29 @@ class NewEmployee extends Component {
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-revers newEmployeeRow" style={{ textAlign: "right" }}>First Name:</div>
-							<div className="col newEmployeeRow" >
+							<div
+								className="col-5 d-flex flex-row-revers newEmployeeRow"
+								style={{ textAlign: "right" }}
+							>
+								First Name:
+							</div>
+							<div className="col newEmployeeRow">
 								<input
 									type="text"
 									value={this.state.firstname}
 									onChange={this.handleFirstnameInput}
 								/>
 								<div className="row errorText">
-									{showFirstnameError && <div className="error"> {FirstnameError} </div>}
+									{showFirstnameError && (
+										<div className="error"> {FirstnameError} </div>
+									)}
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Last Name:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Last Name:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -355,12 +435,16 @@ class NewEmployee extends Component {
 									onChange={this.handleLastnameInput}
 								/>
 								<div className="row errorText">
-									{showLastnameError && <div className="error"> {LastnameError} </div>}
+									{showLastnameError && (
+										<div className="error"> {LastnameError} </div>
+									)}
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Address:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Address:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -368,12 +452,16 @@ class NewEmployee extends Component {
 									onChange={this.handleAddressInput}
 								/>
 								<div className="row errorText">
-									{showAddressError && <div className="error"> {AddressError} </div>}
+									{showAddressError && (
+										<div className="error"> {AddressError} </div>
+									)}
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">City:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								City:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -386,7 +474,9 @@ class NewEmployee extends Component {
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Email:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Email:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="text"
@@ -394,20 +484,26 @@ class NewEmployee extends Component {
 									onChange={this.handleEmailInput}
 								/>
 								<div className="row errorText">
-									{showEmailError && <div className="error"> {EmailError} </div>}
+									{showEmailError && (
+										<div className="error"> {EmailError} </div>
+									)}
 								</div>
 							</div>
 						</div>
 						<div className="row">
-							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">Start Date:</div>
+							<div className="col-5 d-flex flex-row-reverse newEmployeeRow">
+								Start Date:
+							</div>
 							<div className="col newEmployeeRow">
 								<input
 									type="date"
 									value={this.state.startdate}
 									onChange={this.handleStartdateInput}
 								/>
-								<div className="row errorText" >
-									{showStartDateError && <div className="error"> {StartDateError} </div>}
+								<div className="row errorText">
+									{showStartDateError && (
+										<div className="error"> {StartDateError} </div>
+									)}
 								</div>
 							</div>
 						</div>
@@ -458,7 +554,9 @@ class NewEmployee extends Component {
 									onChange={this.handleInstitutionIdInput}
 								/>
 								<div className="row errorText">
-									{showInstituiteIDError && <div className="error"> {InstituiteIDError} </div>}
+									{showInstituiteIDError && (
+										<div className="error"> {InstituiteIDError} </div>
+									)}
 								</div>
 							</div>
 						</div>
@@ -488,7 +586,9 @@ class NewEmployee extends Component {
 									onChange={this.handleTransitIdInput}
 								/>
 								<div className="row errorText">
-									{showTransIDError && <div className="error"> {TransIDError} </div>}
+									{showTransIDError && (
+										<div className="error"> {TransIDError} </div>
+									)}
 								</div>
 							</div>
 						</div>
@@ -497,35 +597,45 @@ class NewEmployee extends Component {
 								Upload a File:
 							</div>
 							<div className="col newEmployeeRow">
-							<div style={{width:"60%"}}>
-								<button
-									type="button"
-									className="btn btn-warning m-2"
-									onClick={this.uploadFile}
-									style={{width: "40%"}}
-									id="employeeCardUploadButton">
-										<input type="file" ref={this.hiddenFileBtn} name="Upload File" onChange={this.saveFile} style={{display:"none"}}/> Upload File
+								<div style={{ width: "60%" }}>
+									<button
+										type="button"
+										className="btn btn-warning m-2"
+										onClick={this.uploadFile}
+										style={{ width: "40%" }}
+										id="employeeCardUploadButton"
+									>
+										<input
+											type="file"
+											ref={this.hiddenFileBtn}
+											name="Upload File"
+											onChange={this.saveFile}
+											style={{ display: "none" }}
+										/>{" "}
+										Upload File
 									</button>
-									{fileSelected?(<div> {this.file.name}</div>):""}
+									{fileSelected ? <div> {this.file.name}</div> : ""}
 								</div>
-								
 							</div>
-						</div><br /><br /><br />
-						{error &&
+						</div>
+						<br />
+						<br />
+						<br />
+						{error && (
 							<div className="row">
 								<div className="col-2"></div>
 
-								<div class=" col-8  alert alert-danger d-flex align-items-center" role="alert">
-									<div>
-										{errorMessage}
-									</div>
+								<div
+									class=" col-8  alert alert-danger d-flex align-items-center"
+									role="alert"
+								>
+									<div>{errorMessage}</div>
 								</div>
 								<div className="col-2"></div>
 							</div>
-
-						}
+						)}
 						<div className="row">
-							<div className="col d-flex justify-content-between newEmployeeRow" >
+							<div className="col d-flex justify-content-between newEmployeeRow">
 								<Link to="/adminEmployees">
 									<button
 										type="button"
